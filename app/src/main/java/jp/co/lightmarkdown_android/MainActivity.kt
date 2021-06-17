1c971fe3d3550d098d7cc338f47e1994ff5cf5f5
sin9990 <rrrtestjc@gmail.com>
2021-06-17 18:30:21 +0900

    編集画面作成（DB周り要反映）

--- app/src/main/java/jp/co/lightmarkdown_android/MainActivity.kt
+++ app/src/main/java/jp/co/lightmarkdown_android/MainActivity.kt
@@ -7,23 +7,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 
+// 画面遷移確認用(スタブ)
 class MainActivity : AppCompatActivity() {
     private lateinit var titleText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(jp.co.lightmarkdown_android.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         val sendButton: Button = findViewById(jp.co.lightmarkdown_android.R.id.send_button)
-        setSupportActionBar(findViewById(R.id.my_toolbar))
-
-        // プリファレンス使ってるのでNG
 
         // タイトル
         titleText = findViewById<EditText>(R.id.titleView2)
 
+        val title = intent.getStringExtra("title")
+        val content = intent.getStringExtra("content")
+        println(title)
+        println(content)
+
         sendButton.setOnClickListener {
             val intent = Intent(application, EditActivity::class.java)
-            intent.putExtra("TITLE", titleText.text.toString())
+            intent.putExtra("id", titleText.text.toString())
             startActivity(intent)
         }
     }
